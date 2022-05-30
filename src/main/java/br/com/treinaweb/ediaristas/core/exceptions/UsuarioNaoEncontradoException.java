0ef557bb465284bb82513e6e4ff62782d8a4819d
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2022-05-30 19:16:31 -0300

    Criacao da exclusao

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/exceptions/UsuarioNaoEncontradoException.java
@@ -0,0 +1,10 @@
+package br.com.treinaweb.ediaristas.core.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+
+public class UsuarioNaoEncontradoException extends EntityNotFoundException {
+
+    public UsuarioNaoEncontradoException(String message) {
+        super(message);
+    }
+}
--- src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
+++ src/main/java/br/com/treinaweb/ediaristas/web/services/WebUsuarioService.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
+import br.com.treinaweb.ediaristas.core.exceptions.UsuarioNaoEncontradoException;
 import br.com.treinaweb.ediaristas.core.models.Usuario;
 import br.com.treinaweb.ediaristas.core.repositories.UsuarioRepository;
+import br.com.treinaweb.ediaristas.web.dtos.UsuarioCadastroForm;
+import br.com.treinaweb.ediaristas.web.mappers.WebUsuarioMapper;
 
 @Service
 public class WebUsuarioService {
@@ -14,7 +18,31 @@ public class WebUsuarioService {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private WebUsuarioMapper mapper;
+
     public List<Usuario> buscarTodos(){
         return repository.findAll();
+    }
+
+    public Usuario cadastrar(UsuarioCadastroForm form) {
+        var model = mapper.toModel(form);
+
+        model.setTipoUsuario(TipoUsuario.ADMIN);
+
+        return repository.save(model);
+    }
+
+    public Usuario buscarPorId(Long id) {
+        var mensagem = String.format("Usuário com ID %d não encontrado", id);
+        
+        return repository.findById(id)
+            .orElseThrow(() -> new UsuarioNaoEncontradoException(mensagem));
+    }
+
+    public void excluirPorId(Long id) {
+        var usuario = buscarPorId(id);
+
+        repository.delete(usuario);
     }
 }

1584a7c4547fae46bbb4acad4dfc04d6c279b52b
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2022-12-18 18:57:23 -0300

    Filtrando Diaristas que atendem a um determinado CEP

--- src/main/java/br/com/treinaweb/ediaristas/core/repositories/UsuarioRepository.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/repositories/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.com.treinaweb.ediaristas.core.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,8 @@ import br.com.treinaweb.ediaristas.core.models.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
+
+    List<Usuario> findByCidadesAtendidasCodigoIbge(String codigoIbge);
 
     @Query("SELECT count(*) > 0 FROM Usuario u WHERE u.email = :email AND (:id is null or u.id != :id)")
     Boolean isEmailJaCadastrado(String email, Long id);

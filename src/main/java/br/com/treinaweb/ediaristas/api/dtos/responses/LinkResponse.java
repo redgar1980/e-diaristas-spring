e70413b994cd3525a6caf33d03a969cfea9b79f3
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-02-03 17:08:21 -0300

    Implementando os HATEOAS

---
+++ src/main/java/br/com/treinaweb/ediaristas/api/dtos/responses/LinkResponse.java
@@ -0,0 +1,19 @@
+package br.com.treinaweb.ediaristas.api.dtos.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkResponse {
+    
+    private String type;
+
+    private String rel;
+
+    private String uri;
+
+
+}

a9a428cff762371cfecae380eebb0d515fe02a92
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-04-26 22:04:04 -0300

    Criando o DTO e Mapper de usuário

---
+++ src/main/java/br/com/treinaweb/ediaristas/api/mappers/ApiUsuarioMapper.java
@@ -0,0 +1,35 @@
+package br.com.treinaweb.ediaristas.api.mappers;
+
+import java.util.stream.Stream;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import br.com.treinaweb.ediaristas.api.dtos.requests.UsuarioRequest;
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
+import br.com.treinaweb.ediaristas.core.models.Usuario;
+
+@Mapper(componentModel = "spring")
+public interface ApiUsuarioMapper {
+
+    ApiUsuarioMapper INSTANCE = Mappers.getMapper(ApiUsuarioMapper.class);
+
+    @Mapping(target = "senha", source = "password")
+    Usuario toModel(UsuarioRequest request);
+
+    default TipoUsuario integerToTipoUsuario(Integer valor) {
+        return Stream.of(TipoUsuario.values())
+            .filter(tipoUsuario -> tipoUsuario.getId().equals(valor))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Tipo Usuário inválido"));
+        // if (valor == 1) {
+        //     return TipoUsuario.CLIENTE;
+        // } else if (valor == 2) {
+        //     return TipoUsuario.DIARISTA;
+        // } else {
+        //     throw new IllegalArgumentException("Tipo Usuário inválido");
+        // }
+    }
+    
+}

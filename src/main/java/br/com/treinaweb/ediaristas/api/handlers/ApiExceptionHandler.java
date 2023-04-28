b6ee3cf5af423b4e0e67e26afd94aebbd674731b
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-04-28 15:16:15 -0300

    Criando o método para tratar os erros de validação na API

--- src/main/java/br/com/treinaweb/ediaristas/api/handlers/ApiExceptionHandler.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/handlers/ApiExceptionHandler.java
@@ -1,20 +1,30 @@
 package br.com.treinaweb.ediaristas.api.handlers;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
 import br.com.treinaweb.ediaristas.api.dtos.responses.ErrorResponse;
 import br.com.treinaweb.ediaristas.core.services.consultaendereco.exceptions.EnderecoServiceException;
 
 @RestControllerAdvice(annotations = RestController.class)
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private SnakeCaseStrategy camelCaseToSnakeCase;
     
     @ExceptionHandler(EnderecoServiceException.class)
     public ResponseEntity<Object> handleEnderecoServiceException(
@@ -28,5 +38,31 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .build();
             
             return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpHeaders headers, 
+            HttpStatus status, 
+            WebRequest request
+    ) {
+        var body = new HashMap<String, ArrayList<String>>();
+        
+        exception.getBindingResult().getFieldErrors()
+            .forEach(fieldError -> {
+                var field = camelCaseToSnakeCase.translate(fieldError.getField());
+
+                if (!body.containsKey(field)) {
+                    var fieldErrors = new ArrayList<String>();
+                    fieldErrors.add(fieldError.getDefaultMessage());
+                    
+                    body.put(field, fieldErrors);
+                } else {
+                    body.get(field).add(fieldError.getDefaultMessage());
+                }
+            });
+
+        return ResponseEntity.badRequest().body(body); 
     }
 }
---
+++ src/main/java/br/com/treinaweb/ediaristas/api/handlers/List.java
@@ -0,0 +1,5 @@
+package br.com.treinaweb.ediaristas.api.handlers;
+
+public class List<T> {
+
+}

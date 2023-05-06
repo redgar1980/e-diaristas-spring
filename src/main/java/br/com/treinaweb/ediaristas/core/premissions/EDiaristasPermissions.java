83664a89d2cd641a52cab4d25748699c27c77260
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-05-06 19:03:52 -0300

    Criando classes auxiliares para o processo de autorização

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/premissions/EDiaristasPermissions.java
@@ -0,0 +1,16 @@
+package br.com.treinaweb.ediaristas.core.premissions;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+public @interface EDiaristasPermissions {
+    
+    @PreAuthorize("hasAnyAuthority('CLIENTE', 'DIARISTA')")
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.METHOD)
+    public @interface isDiaristaOrCliente {}
+}

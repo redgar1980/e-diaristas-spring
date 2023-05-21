35d059c0cd5c7d28c2870d0ff58df531f0a09f1d
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-05-21 20:36:04 -0300

    Atualizando a configuração do Spring Security

--- src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfig.java
@@ -6,16 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -23,127 +21,106 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
 import br.com.treinaweb.ediaristas.core.filters.AccessTokenRequestFilter;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    
+    @Value("${br.com.treinaweb.ediaristas.rememberMe.key}")
+    private String rememberMeKey;
+    
+    @Value("${br.com.treinaweb.ediaristas.rememberMe.validitySeconds}")
+    private int rememberMeValiditySeconds;
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private AccessTokenRequestFilter accessTokenRequestFilter;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private AuthenticationEntryPoint authenticationEntryPoint;
 
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Bean
     @Order(1)
-    @Configuration
-    public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.requestMatchers(requestMatcherCustomizer ->
+            requestMatcherCustomizer
+                .antMatchers("/api/**", "/auth/**")
+        )
+        .authorizeRequests(authorizeRequestsCustomizer -> 
+            authorizeRequestsCustomizer
+                .anyRequest()
+                .permitAll()
+        )
+        .csrf(csrfCustomizer ->
+            csrfCustomizer
+                .disable()
+        )
+        .sessionManagement(sessionManagmentCustomizer -> 
+            sessionManagmentCustomizer
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        )
+        .addFilterBefore(accessTokenRequestFilter, UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling(exceptionHandlingCustomizer -> 
+            exceptionHandlingCustomizer
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+        )
+        .cors();
 
-        @Autowired
-        private AccessTokenRequestFilter accessTokenRequestFilter;
-
-        @Autowired
-        private AuthenticationEntryPoint authenticationEntryPoint;
-
-        @Autowired
-        private AccessDeniedHandler accessDeniedHandler;
-
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.requestMatchers(requestMatcherCustomizer ->
-                requestMatcherCustomizer
-                    .antMatchers("/api/**", "/auth/**")
-            )
-            .authorizeRequests(authorizeRequestsCustomizer -> 
-                authorizeRequestsCustomizer
-                    .anyRequest()
-                    .permitAll()
-            )
-            .csrf(csrfCustomizer ->
-                csrfCustomizer
-                    .disable()
-            )
-            .sessionManagement(sessionManagmentCustomizer -> 
-                sessionManagmentCustomizer
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .addFilterBefore(accessTokenRequestFilter, UsernamePasswordAuthenticationFilter.class)
-            .exceptionHandling(exceptionHandlingCustomizer -> 
-                exceptionHandlingCustomizer
-                    .authenticationEntryPoint(authenticationEntryPoint)
-                    .accessDeniedHandler(accessDeniedHandler)
-            )
-            .cors();
-        }
-
-        @Bean
-        @Override
-        public AuthenticationManager authenticationManagerBean() throws Exception {
-            return super.authenticationManagerBean();
-        }
+        return http.build();
     }
-
+    
+    @Bean
     @Order(2)
-    @Configuration
-    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
         
-        @Value("${br.com.treinaweb.ediaristas.rememberMe.key}")
-        private String rememberMeKey;
-    
-        @Value("${br.com.treinaweb.ediaristas.rememberMe.validitySeconds}")
-        private int rememberMeValiditySeconds;
+        http.requestMatchers(requestMatcherCustomizer -> 
+            requestMatcherCustomizer
+                .antMatchers("/admin/**")
+        )
+        .authorizeRequests(authorizeRequestsCustomizer -> 
+            authorizeRequestsCustomizer
+                .anyRequest()
+                .hasAuthority(TipoUsuario.ADMIN.name())
+        )
+        .formLogin(formLoginCustomizer -> 
+            formLoginCustomizer
+                .loginPage("/admin/login")
+                .usernameParameter("email")
+                .passwordParameter("senha")
+                .defaultSuccessUrl("/admin/servicos")
+                .permitAll()
+        )
+        .logout(logoutCustomizer -> 
+            logoutCustomizer
+                .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET"))
+                .logoutSuccessUrl("/admin/login")
+        )
+        .rememberMe(rememberMeCustomizer ->
+            rememberMeCustomizer
+                .rememberMeParameter("lembrar-me")
+                .tokenValiditySeconds(rememberMeValiditySeconds)
+                .key(rememberMeKey)
+        )
+        .exceptionHandling(exceptionHandlingCustomizer -> 
+            exceptionHandlingCustomizer
+                .accessDeniedPage("/admin/login")
+        );
 
-        @Override
-        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);
-        }
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.requestMatchers(requestMatcherCustomizer -> 
-                requestMatcherCustomizer
-                    .antMatchers("/admin/**")
-            )
-            .authorizeRequests(authorizeRequestsCustomizer -> 
-                authorizeRequestsCustomizer
-                    .anyRequest()
-                    .hasAuthority(TipoUsuario.ADMIN.name())
-            )
-            .formLogin(formLoginCustomizer -> 
-                formLoginCustomizer
-                    .loginPage("/admin/login")
-                    .usernameParameter("email")
-                    .passwordParameter("senha")
-                    .defaultSuccessUrl("/admin/servicos")
-                    .permitAll()
-            )
-            .logout(logoutCustomizer -> 
-                logoutCustomizer
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET"))
-                    .logoutSuccessUrl("/admin/login")
-            )
-            .rememberMe(rememberMeCustomizer ->
-                rememberMeCustomizer
-                    .rememberMeParameter("lembrar-me")
-                    .tokenValiditySeconds(rememberMeValiditySeconds)
-                    .key(rememberMeKey)
-            )
-            .exceptionHandling(exceptionHandlingCustomizer -> 
-                exceptionHandlingCustomizer
-                    .accessDeniedPage("/admin/login")
-            );
-        }
-
-        public void configure(WebSecurity web) throws Exception {
-            web.ignoring()
-                .antMatchers("/webjars/**")
-                .antMatchers("/img/**");
-        }
+        return http.build();
     }
-    
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+            .antMatchers("/webjars/**")
+            .antMatchers("/img/**");
+    }
+
+    @Bean
+    public AuthenticationManager authorizationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
---
+++ src/main/java/br/com/treinaweb/ediaristas/config/SecurityConfigOld.java
@@ -0,0 +1,149 @@
+package br.com.treinaweb.ediaristas.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
+import br.com.treinaweb.ediaristas.core.filters.AccessTokenRequestFilter;
+
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfigOld {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+   // @Order(1)
+    //@Configuration
+    public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private AccessTokenRequestFilter accessTokenRequestFilter;
+
+        @Autowired
+        private AuthenticationEntryPoint authenticationEntryPoint;
+
+        @Autowired
+        private AccessDeniedHandler accessDeniedHandler;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.requestMatchers(requestMatcherCustomizer ->
+                requestMatcherCustomizer
+                    .antMatchers("/api/**", "/auth/**")
+            )
+            .authorizeRequests(authorizeRequestsCustomizer -> 
+                authorizeRequestsCustomizer
+                    .anyRequest()
+                    .permitAll()
+            )
+            .csrf(csrfCustomizer ->
+                csrfCustomizer
+                    .disable()
+            )
+            .sessionManagement(sessionManagmentCustomizer -> 
+                sessionManagmentCustomizer
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .addFilterBefore(accessTokenRequestFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(exceptionHandlingCustomizer -> 
+                exceptionHandlingCustomizer
+                    .authenticationEntryPoint(authenticationEntryPoint)
+                    .accessDeniedHandler(accessDeniedHandler)
+            )
+            .cors();
+        }
+
+        @Bean
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+    }
+
+   // @Order(2)
+   // @Configuration
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        
+        @Value("${br.com.treinaweb.ediaristas.rememberMe.key}")
+        private String rememberMeKey;
+    
+        @Value("${br.com.treinaweb.ediaristas.rememberMe.validitySeconds}")
+        private int rememberMeValiditySeconds;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.requestMatchers(requestMatcherCustomizer -> 
+                requestMatcherCustomizer
+                    .antMatchers("/admin/**")
+            )
+            .authorizeRequests(authorizeRequestsCustomizer -> 
+                authorizeRequestsCustomizer
+                    .anyRequest()
+                    .hasAuthority(TipoUsuario.ADMIN.name())
+            )
+            .formLogin(formLoginCustomizer -> 
+                formLoginCustomizer
+                    .loginPage("/admin/login")
+                    .usernameParameter("email")
+                    .passwordParameter("senha")
+                    .defaultSuccessUrl("/admin/servicos")
+                    .permitAll()
+            )
+            .logout(logoutCustomizer -> 
+                logoutCustomizer
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET"))
+                    .logoutSuccessUrl("/admin/login")
+            )
+            .rememberMe(rememberMeCustomizer ->
+                rememberMeCustomizer
+                    .rememberMeParameter("lembrar-me")
+                    .tokenValiditySeconds(rememberMeValiditySeconds)
+                    .key(rememberMeKey)
+            )
+            .exceptionHandling(exceptionHandlingCustomizer -> 
+                exceptionHandlingCustomizer
+                    .accessDeniedPage("/admin/login")
+            );
+        }
+
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring()
+                .antMatchers("/webjars/**")
+                .antMatchers("/img/**");
+        }
+    }
+    
+}

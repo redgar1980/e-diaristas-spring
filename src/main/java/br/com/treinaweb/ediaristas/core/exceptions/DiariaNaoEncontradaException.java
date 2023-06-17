35bf60abe5367f5b4bb1476f4ea1686ec9348c8b
Rodrigo Edgar Custodio <e_custodio2002@hotmail.com>
2023-06-16 21:51:50 -0300

    Criando o DTO, controller e service de pagamento

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/exceptions/DiariaNaoEncontradaException.java
@@ -0,0 +1,10 @@
+package br.com.treinaweb.ediaristas.core.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+
+public class DiariaNaoEncontradaException extends EntityNotFoundException{
+    
+    public DiariaNaoEncontradaException(String message) {
+        super(message);
+    }
+}

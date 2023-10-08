4f866d3c581d1b271d5c72dd4393980fe9b19a17
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2023-10-08 15:40:23 -0300

    Adicionando as rotas de detalhes e alteração do Endereço no HATEOAS

--- src/main/java/br/com/treinaweb/ediaristas/api/assemblers/UsuarioAssembler.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/assemblers/UsuarioAssembler.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import br.com.treinaweb.ediaristas.api.controllers.DiariaRestController;
+import br.com.treinaweb.ediaristas.api.controllers.EnderecoDiaristaRestController;
 import br.com.treinaweb.ediaristas.api.dtos.responses.UsuarioResponse;
 
 @Component
@@ -21,6 +22,16 @@ public class UsuarioAssembler implements Assembler<UsuarioResponse> {
                 .withType("POST");
 
                 resource.adicionarLinks(cadastrarDiariaLink);
+        } else {
+            var atualizarEnderecoLink = linkTo(methodOn(EnderecoDiaristaRestController.class).alterarEndereco(null))
+                .withRel("atualizar_endereco")
+                .withType("PUT");
+            
+            var listarEnderecoLink = linkTo(methodOn(EnderecoDiaristaRestController.class).exibirEndereco())
+                .withRel("listar_endereco")
+                .withType("GET");
+            
+            resource.adicionarLinks(atualizarEnderecoLink, listarEnderecoLink);
         }
 
         var listaDiariasLink = linkTo(methodOn(DiariaRestController.class).listarPorUsuarioLogado())
@@ -31,6 +42,7 @@ public class UsuarioAssembler implements Assembler<UsuarioResponse> {
     }
 
     @Override
-    public void adicionarLinks(List<UsuarioResponse> collectionResource) {}
-    
+    public void adicionarLinks(List<UsuarioResponse> collectionResource) {
+    }
+
 }

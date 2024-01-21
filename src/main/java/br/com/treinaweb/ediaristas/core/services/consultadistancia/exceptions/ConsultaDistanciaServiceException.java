0ade27a0128a00b743288ecd958eabc76f37814a
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-01-21 20:13:28 -0300

    Criando o service de cálculo de distância parte 1

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/services/consultadistancia/exceptions/ConsultaDistanciaServiceException.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.ediaristas.core.services.consultadistancia.exceptions;
+
+public class ConsultaDistanciaServiceException extends RuntimeException {
+
+  public ConsultaDistanciaServiceException() {
+
+  }
+
+  public ConsultaDistanciaServiceException(String message) {
+    super(message);
+  }
+
+}

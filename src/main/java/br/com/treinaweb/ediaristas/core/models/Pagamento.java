c21d61887354e6fc05c2ee8094da99eff16a5115
Rodrigo Custodio <rodrigocustodio2023@hotmail.com>
2024-03-22 16:57:26 -0300

    Criando o service de integração com o gateway de pagamento na rota de pagamento

--- src/main/java/br/com/treinaweb/ediaristas/core/models/Pagamento.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/models/Pagamento.java
@@ -14,6 +14,7 @@ import javax.persistence.ManyToOne;
 
 import br.com.treinaweb.ediaristas.core.enums.PagamentoStatus;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -24,6 +25,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@Builder
 public class Pagamento extends Auditable {
 
     @Id
--- src/main/java/br/com/treinaweb/ediaristas/core/services/gatewaypagamento/providers/PagarMeService.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/services/gatewaypagamento/providers/PagarMeService.java
@@ -1,15 +1,25 @@
 package br.com.treinaweb.ediaristas.core.services.gatewaypagamento.providers;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import br.com.treinaweb.ediaristas.core.enums.PagamentoStatus;
 import br.com.treinaweb.ediaristas.core.models.Diaria;
 import br.com.treinaweb.ediaristas.core.models.Pagamento;
+import br.com.treinaweb.ediaristas.core.repositories.PagamentoRepository;
 import br.com.treinaweb.ediaristas.core.services.gatewaypagamento.adapters.GatewayPagamentoService;
+import br.com.treinaweb.ediaristas.core.services.gatewaypagamento.exceptions.GatewayPagamentoException;
+import br.com.treinaweb.ediaristas.core.services.gatewaypagamento.providers.dtos.PagarMeTransacaoRequest;
+import br.com.treinaweb.ediaristas.core.services.gatewaypagamento.providers.dtos.PagarMeTransacaoResponse;
 
 @Service
 public class PagarMeService implements GatewayPagamentoService {
@@ -22,11 +32,74 @@ public class PagarMeService implements GatewayPagamentoService {
     private String apiKey;
 
     @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
     private ObjectMapper objectMapper;
 
     @Override
     public Pagamento pagar(Diaria diaria, String cardHash) {
-        return null;
+        try {
+            return tryPagar(diaria, cardHash);
+        } catch (HttpClientErrorException.BadRequest exception) {
+            var responseBody = exception.getResponseBodyAsString();
+            var jsonNode = getJsonNode(responseBody);
+            var message = jsonNode.path("errors")
+                    .path(0)
+                    .path("message")
+                    .asText();
+
+            throw new GatewayPagamentoException(message);
+        }
+    }
+
+    private Pagamento tryPagar(Diaria diaria, String cardHash) {
+        var transacaoRequest = criarTransacaoResquest(diaria, cardHash);
+        var url = BASE_URL + "/transactions";
+        var response = clienteHttp.postForEntity(url, transacaoRequest, PagarMeTransacaoResponse.class);
+        return criarPagamento(diaria, response.getBody());
+    }
+
+    @SuppressWarnings("null")
+    private Pagamento criarPagamento(Diaria diaria, PagarMeTransacaoResponse body) {
+        var pagamento = Pagamento.builder()
+                .valor(diaria.getPreco())
+                .transacaoId(body.getId())
+                .diaria(diaria)
+                .status(criarPagamentoStatus(body.getStatus()))
+                .build();
+        return pagamentoRepository.save(pagamento);
+    }
+
+    private PagamentoStatus criarPagamentoStatus(String transacaoStatus) {
+        /*
+         * if (transacaoStatus.equals("paid")) {
+         * return PagamentoStatus.ACEITO;
+         * }
+         * return PagamentoStatus.REPROVADO;
+         */
+        return transacaoStatus.equals("paid") ? PagamentoStatus.ACEITO : PagamentoStatus.REPROVADO;
+    }
+
+    private PagarMeTransacaoRequest criarTransacaoResquest(Diaria diaria, String cardHash) {
+        return PagarMeTransacaoRequest.builder()
+                .amount(converterReaisParaCentavos(diaria.getPreco()))
+                .cardHash(cardHash)
+                .async(false)
+                .apiKey(apiKey)
+                .build();
+    }
+
+    private Integer converterReaisParaCentavos(BigDecimal preco) {
+        return preco.multiply(new BigDecimal(100)).intValue();
+    }
+
+    private JsonNode getJsonNode(String responseBody) {
+        try {
+            return objectMapper.readTree(responseBody);
+        } catch (JsonProcessingException exception) {
+            throw new GatewayPagamentoException(exception.getLocalizedMessage());
+        }
     }
 
 }
